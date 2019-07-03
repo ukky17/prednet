a000bbfcad74231fa3c535b4863190877f4315a8
ukky17 <i.love.ny517@gmail.com>
2019-07-03 15:46:38 +0900

    fix for large dataset

--- kitti_data_converter/converter2.py
+++ kitti_data_converter/converter2.py
@@ -1,11 +1,11 @@
 import os
 import hickle
-import pickle
+import numpy as np
 
-parent = '/export/public/ukita/PredNet/prednet/kitti_data/'
+parent = '/export/public/ukita/prednet/kitti_data/'
 
-filenames = ['X_val', 'X_test', 'sources_val', 'sources_test']
+filenames = ['X_train', 'X_val', 'X_test', 'sources_train', 'sources_val', 'sources_test']
 
 for filename in filenames:
-    tmp = pickle.load(open(parent + filename + '.pkl', 'rb'), encoding='latin1')
+    tmp = np.load(parent + filename + '.npy')
     hickle.dump(tmp, parent + filename + '.hkl', mode='w')
