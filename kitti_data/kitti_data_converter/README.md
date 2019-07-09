9248c0ecab7ab3f4a2d3293780ca6e0351957a23
ukky17 <i.love.ny517@gmail.com>
2019-07-09 21:55:33 +0900

    add shape_converte.py

--- kitti_data/kitti_data_converter/README.md
+++ kitti_data/kitti_data_converter/README.md
@@ -1,5 +1,9 @@
-misc files to converter .hkl files (created in python2 and old hickle version) to new files
+# misc files to convert .hkl files (created in python2 and old hickle version) to new files
 
-converter1: open in python2
+converter1.py: open in python2
 
-converter2: open in python3
+converter2.py: open in python3
+
+# shape converter
+
+shape_converter.py
---
+++ kitti_data/kitti_data_converter/shape_converter.py
@@ -0,0 +1,23 @@
+import os
+import hickle
+import numpy as np
+import cv2
+
+target_size = (128, 192)
+
+parent = '/export/public/ukita/prednet/kitti_data/'
+
+filenames = ['X_train', 'X_val', 'X_test', 'sources_train', 'sources_val', 'sources_test']
+
+for filename in filenames:
+    tmp = hickle.load(parent + filename + '.hkl')
+
+    if 'sources' in filename:
+        tmp_converted = tmp
+    else:
+        tmp_converted = np.zeros((len(tmp), ) + target_size + (3, ), dtype=tmp.dtype)
+        for i in range(len(tmp)):
+            tmp_converted[i] = cv2.resize(tmp[i], (target_size[1], target_size[0]))
+
+    hickle.dump(tmp_converted, parent + filename + str(target_size[0]) + 'x' +
+                               str(target_size[1]) + '.hkl', mode='w')
